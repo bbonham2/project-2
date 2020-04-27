42964dd62648b6d838926ac21ea8019e9c4ff8f7
bbonham2 <bbonham2@uoregon.edu>
2020-04-27 00:27:47 -0500

    flickr image gallery

--- client/javascripts/gallery.js
+++ client/javascripts/gallery.js
@@ -1,19 +1,21 @@
 // jshint esversion: 6
 
-let main = function(){
+let controller = function() {
+  let prefixURL =
+    "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
+  let suffixURL = "&format=json&jsoncallback=?";
+  //get value entered by user from textbox
+  let flickrTag = $("input").val();
+  console.log(flickrTag);
+  let requestURL = prefixURL + flickrTag + suffixURL;
+  console.log(requestURL);
 
-    let prefixURL = "http://api.flickr.com/services/feeds/photos_public.gne?tags=";
-    let suffixURL = "&format=json&jsoncallback=?";
-    //get value entered by user from textbox
-    let flickrTag = $("input").???();
-    let requestURL = prefixURL + flickrTag + suffixURL;
-
-    //clear old photos
-    $(".photos").???("");
+  document.querySelector(".photos").innerHTML = "";
+  //clear old photos
+  //$(".photos").???("");
 
   $.getJSON(requestURL, function(flickrResponse) {
     flickrResponse.items.forEach(function(item, index) {
-
       //Flickr returns 20 images by default
       //We need only six images for the Gallery
       if (index < 6) {
@@ -24,7 +26,7 @@ let main = function(){
         // set the attribute to the url
         // contained in the response
         $img.attr("src", item.media.m);
-        $img.attr("width", "100");
+        $img.attr("width", "150");
 
         // attach the img tag to the main
         // photos element and then fade it in
@@ -32,9 +34,12 @@ let main = function(){
         $img.fadeIn();
       }
     });
-
   });
 };
 
+//$(document).ready(controller);
 
-$(document).ready(main);
+window.addEventListener("load", () => {
+  let button = document.querySelector("button");
+  button.addEventListener("click", controller);
+});
